e7d71e85261a64ac7e615e5018d924c99e7d0f47
9ziggy9 <davidarogers@protonmail.com>
2024-03-06 11:37:30 -0800

    in hell trying to pad this data structure

--- ocaml-complex-numbers/main.ml
+++ ocaml-complex-numbers/main.ml
@@ -59,7 +59,7 @@ module type Grid = sig
   val make      : t list list -> grid
   val iterate   : grid -> (t -> unit) -> unit
   val string_of : grid -> string 
-  val map       : grid -> (t -> t) -> grid
+  val map       : (t -> t) -> grid -> grid
   val (@@)      : grid -> int * int -> t option
 end
 
@@ -70,9 +70,10 @@ end
 (* This is the reason for the `with type t = G.data` constraint in addition *)
 (* to type t assignment (which satisfies Grid interface requirements).      *)
 (* If you want to see difference, uncomment functor signature and observe.  *)
-(* module GridMapping (G : GridBasis) : Grid = struct *)
-module GridMapping (G : GridBasis) : Grid with type t = G.data = struct
-  type t = G.data
+(* module GridMapping (B : GridBasis) : Grid = struct *)
+module GridMapping (Basis : GridBasis) : Grid with type t = Basis.data = struct
+  open Basis
+  type t = data
   type grid = PrivateGrid of grid_internal
   and grid_internal =
     | Row of row_internal * grid_internal
@@ -82,7 +83,12 @@ module GridMapping (G : GridBasis) : Grid with type t = G.data = struct
     | NoCell
 
   let option_of_void (x : t) : t option =
-    if G.should_void x then None else Some x
+    if should_void x then None else Some x
+
+  let d_lengths (rows : t list list) : int list =
+    let candidate_lengths = List.map (fun row -> List.length row) rows in
+    let min_width = List.fold_left max 0 candidate_lengths in
+    List.map (fun l -> min_width - l) candidate_lengths
 
   let rec make (rows : t list list) : grid =
     let rec make_row (xs : t list) : row_internal =
@@ -110,31 +116,42 @@ module GridMapping (G : GridBasis) : Grid with type t = G.data = struct
   let string_of (PrivateGrid g) : string =
     let rec string_of_row (r : row_internal) = match r with
       | NoCell            -> ""
-      | Cell (Some x, xs) -> begin match G.string_from with
+      | Cell (Some x, xs) -> begin match string_from with
                                 | Some f -> " " ^ f x ^ " "
                                 | None   -> " <?> "
                              end ^ string_of_row xs
-      | Cell (None, xs)   -> " _ " ^ string_of_row xs in
-    let rec string_of_grid (g : grid_internal) = match g with
+      | Cell (None, xs)   -> " _ " ^ string_of_row xs
+    and string_of_grid (g : grid_internal) = match g with
       | NoRow       -> ""
       | Row (r, rs) -> string_of_row r ^ "\n" ^ string_of_grid rs
     in string_of_grid g
 
-  let map (PrivateGrid g) f =
+  (* endomorphic variant ! *)
+  let map (f: t -> t) (PrivateGrid g) : grid =
     let rec map_row (r : row_internal) = match r with
       | NoCell            -> NoCell
       | Cell (Some x, xs) -> Cell (Some (f x), map_row xs)
       | Cell (None, xs)   -> Cell (None, map_row xs)
-    in
-    let rec map_grid (g : grid_internal) = match g with
+    and map_grid (g : grid_internal) = match g with
       | NoRow       -> NoRow
       | Row (r, rs) -> Row (map_row r, map_grid rs)
-    in
-    PrivateGrid (map_grid g)
+    in PrivateGrid (map_grid g)
+
+  let fold (f: 'acc -> t -> 'acc) (init : 'acc) (PrivateGrid g) : 'acc =
+    let rec fold_grid (acc : 'acc) (g : grid_internal) = match g with
+      | Row (r, rs) -> fold_grid (fold_row acc r) rs
+      | NoRow       -> acc
+    and fold_row (acc : 'acc) (r : row_internal) = match r with
+      | NoCell -> acc
+      | _ -> fold_cell acc r
+    and fold_cell (acc: 'acc) (r: row_internal) = match r with
+      | Cell(Some x, xs) -> fold_cell (f acc x) xs
+      | Cell(None, xs)   -> fold_cell acc xs
+      | NoCell           -> acc
+    in fold_grid init g
 
   (* What happens if I have a name collision with this shit? *)
-  let (@@) (grid : grid) (coords : int * int) : t option =
-    let (x, y) = coords in
+  let (@@) (grid : grid) ((x, y) : int * int) : t option =
     let rec find_row (g : grid_internal) (x : int) = match g with
       | NoRow       -> None
       | Row (r, rs) -> if x = 0 then Some r else find_row rs (x - 1)
